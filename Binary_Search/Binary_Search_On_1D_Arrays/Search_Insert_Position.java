0898baaa785c37a004f26c5b2f5bc9e61d849e45
niks-nikhil-anand <niks.anand.developer@gmail.com>
2023-12-10 20:39:28 +0530

    Find Bad Version

--- Binary_Search/Binary_Search_On_1D_Arrays/Search_Insert_Position.java
+++ Binary_Search/Binary_Search_On_1D_Arrays/Search_Insert_Position.java
@@ -26,3 +26,33 @@ class Solution {
     return ans ;    
     }
 }
+
+
+// Similar Problem :- First Bad Version
+
+// Problem Statement :- You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+// You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+         int start = 1;
+         int end = n;
+         int bad = -1;
+         while(start<=end){
+             int mid = start+(end-start)/2;
+           
+             if(isBadVersion(mid)){ 
+                 bad = mid;
+                 end = mid-1;
+             }else { 
+                 start = mid+1;
+             }
+         }
+         return bad ;   
+        }
+    }
